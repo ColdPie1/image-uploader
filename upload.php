91d50a61c817249c8ebb4564ea33d31984879d1d
Andrew Eikum <coldpie@fastmail.com>
2024-08-10 07:17:41 -0500

    implement gallery

--- upload.php
+++ upload.php
@@ -52,7 +52,19 @@ function copy(name) {
 </head>
 <body>
 
-<pre><?php
+<?php
+
+function compress($str)
+{
+    return "gz:" . rawurlencode(base64_encode(gzcompress($str)));
+}
+
+function uncompress($str)
+{
+    if(!str_starts_with($str, "gz:"))
+        return $str;
+    return gzuncompress(base64_decode(rawurldecode(substr($str, 3))));
+}
 
 function is_checked($id)
 {
@@ -298,9 +310,9 @@ function make_smaller($filename, $orientation)
     echo("Saving $w" . "x$h image at \"$fname_t\".\n");
 
     if($ext == "jpg"){
-        imagejpeg($img_s, $fname_s);
+        imagejpeg($img_s, $fname_s, 95);
         merge_metadata($fname_s, $filename);
-        imagejpeg($img_t, $fname_t);
+        imagejpeg($img_t, $fname_t, 95);
         merge_metadata($fname_t, $filename);
     }else if($ext == "png"){
         imagepng($img_s, $fname_s);
@@ -419,133 +431,219 @@ function do_upload()
     echo("Upload complete.\n");
 }
 
+function gen_autogallery($filename)
+{
+    $pivot = strrpos($filename, ".");
+    $base = substr($filename, 0, $pivot);
+    $suffix = substr($filename, $pivot + 1);
+
+    $chunks = explode("_", $base);
+
+    $len = sizeof($chunks);
+    if($len < 3)
+        return false;
+
+    $size_spec = $chunks[$len - 1];
+    if($size_spec == "t" || $size_spec == "s"){
+        $size_spec = "_$size_spec";
+        $idx = $chunks[$len - 2];
+        $date = $chunks[$len - 3];
+        $substr = implode("_", array_slice($chunks, 0, $len - 2));
+    }else{
+        $size_spec = "";
+        $idx = $chunks[$len - 1];
+        $date = $chunks[$len - 2];
+        $substr = implode("_", array_slice($chunks, 0, $len - 1));
+    }
+
+    //echo("got idx $idx and date $date\n");
+    if(preg_match("/\\d+/", $idx) && preg_match("/\\d\\d\\d\\d\\d\\d\\d\\d/", $date)){
+     //   echo("globbing $substr*$size_spec.{jpg,png,webp,gif}\n");
+        $filenames = glob("$substr*$size_spec.{jpg,png,webp,gif}", GLOB_BRACE);
+        natcasesort($filenames);
+        $out = [];
+        foreach($filenames as $f){
+            if(preg_match("/{$substr}_\\d+{$size_spec}\\..*/", $f)){ // filter out ones that don't match size_spec
+                $out[] = $f;
+            }
+        }
+        return implode(",", $out);
+    }
+
+    return false;
+}
+
 function secret_valid($pw1, $pw2)
 {
     /* TODO: implement your clever auth idea here */
     return false;
 }
 
-//print_r($_POST);
-//print_r($_FILES);
-$passwd1_default = "";
-$passwd2_default = "";
-$rename_default = "";
-$sequence_default = "";
-$overwrite_default = "";
-$add_date_default = " checked";
-$do_list = false;
+function do_main()
+{
+?>
+<pre>
+<?php
+    //print_r($_POST);
+    //print_r($_FILES);
+    $passwd1_default = "";
+    $passwd2_default = "";
+    $rename_default = "";
+    $sequence_default = "";
+    $overwrite_default = "";
+    $add_date_default = " checked";
+    $do_list = false;
+    global $MY_URL;
 
-if(isset($_POST["passwd1"])){
-    $passwd1 = trim($_POST["passwd1"]);
-    $passwd2 = trim($_POST["passwd2"]);
+    if(isset($_POST["passwd1"])){
+        $passwd1 = trim($_POST["passwd1"]);
+        $passwd2 = trim($_POST["passwd2"]);
 
-    if(secret_valid($passwd1, $passwd2)){
-        $do_list = true;
-        $seq = get_sequence();
+        if(secret_valid($passwd1, $passwd2)){
+            $do_list = true;
+            $seq = get_sequence();
 
-        if(isset($_FILES["img"]) &&
-                $_FILES["img"]["size"] > 0){
-            do_upload();
+            if(isset($_FILES["img"]) &&
+                    $_FILES["img"]["size"] > 0){
+                do_upload();
+
+                if($seq >= 0){
+                    $seq++;
+                }
+            }
+
+            $passwd1_default = " value=\"$passwd1\"";
+            $passwd2_default = " value=\"$passwd2\"";
+            $rename_default = " value=\"" . trim($_POST["rename"]) . "\"";
+            $overwrite_default = is_checked("overwrite") ? " checked" : "";
+            $add_date_default = is_checked("add_date") ? " checked" : "";
 
             if($seq >= 0){
-                $seq++;
+                $sequence_default = " value=\"" . $seq . "\"";
             }
+        }else{
+            echo("Invalid secret.");
         }
+    }
+?>
+</pre>
 
-        $passwd1_default = " value=\"$passwd1\"";
-        $passwd2_default = " value=\"$passwd2\"";
-        $rename_default = " value=\"" . trim($_POST["rename"]) . "\"";
-        $overwrite_default = is_checked("overwrite") ? " checked" : "";
-        $add_date_default = is_checked("add_date") ? " checked" : "";
+<?php
 
-        if($seq >= 0){
-            $sequence_default = " value=\"" . $seq . "\"";
+    if(isset($filenames_to_copy)){
+        $full_fname = $filenames_to_copy[0][1];
+        $full_url = $MY_URL . rawurlencode($filenames_to_copy[0][1]);
+?>
+        <label><a href="<?php echo($full_url); ?>"><?php echo($full_fname); ?></a></label><br>
+        <label>URL:</label>
+<?php
+        foreach($filenames_to_copy as $fname){
+            $display = $fname[0];
+            $name = $MY_URL . rawurlencode($fname[1]);
+?>
+            <input style="display:none;" type="text" readonly id="<?php echo($display); ?>url" value="<?php echo($name); ?>">
+            <button onclick="copy('<?php echo($display); ?>url')"><?php echo($display); ?></button>
+<?php
         }
-    }else{
-        echo("Invalid secret.");
+?>
+
+        <br><label>[img]:</label>
+<?php
+        foreach($filenames_to_copy as $fname){
+            $display = $fname[0];
+            $name = "[img]" . $MY_URL . rawurlencode($fname[1]) . "[/img]";
+?>
+            <input style="display:none;" type="text" readonly id="<?php echo($display); ?>phpbb" value="<?php echo($name); ?>">
+            <button onclick="copy('<?php echo($display); ?>phpbb')"><?php echo($display); ?></button>
+<?php
+        }
+?>
+    <hr>
+<?php
+    }
+?>
+
+    <form method="post" enctype="multipart/form-data">
+    <label>Secret</label><br>
+    <input type="text" <?php echo($passwd1_default); ?> id="passwd1" name="passwd1"><br>
+    <input type="text"<?php echo($passwd2_default); ?> id="passwd2" name="passwd2"><br>
+
+    <label for="rename">Rename?</label><br>
+    <input type="text"<?php echo($rename_default); ?> id="rename" name="rename" autocapitalize="off"><br>
+
+    <label for="sequence">Sequence?</label><br>
+    <input type="number"<?php echo($sequence_default); ?> id="sequence" name="sequence"><br>
+
+    <label for="overwrite">Overwrite?</label>
+    <input type="checkbox" id="overwrite" name="overwrite"<?php echo($overwrite_default); ?>><br>
+
+    <label for="add_date">Date?</label>
+    <input type="checkbox" id="add_date" name="add_date"<?php echo($add_date_default); ?>><br>
+
+    <input type="file" id="img" name="img" accept="image/png,image/jpeg,image/webp,image/gif" /><br>
+
+    <input type="submit" value="Submit" />
+
+    </form>
+
+<?php
+    if($do_list){
+?>
+    <hr>
+<?php
+        $idx = 0;
+        $filenames = glob("*.{jpg,png,webp,gif}", GLOB_BRACE);
+        natcasesort($filenames);
+        foreach($filenames as $filename){
+            $url = $MY_URL . rawurlencode($filename);
+            $phpbb = "[img]" . $url . "[/img]";
+            $autogallery = gen_autogallery($filename);
+            if($autogallery)
+                $autogallery = $MY_URL . "upload.php?g=" . compress($autogallery);
+            $id = "btn" . $idx;
+            $idx++;
+?>
+        <label><a href="<?php echo($url); ?>"><?php echo($filename); ?></a></label>
+        <button onclick="copy('<?php echo($id); ?>url')">URL</button>
+        <button onclick="copy('<?php echo($id); ?>phpbb')">[img]</button>
+        <input style="display:none;" type="text" readonly id="<?php echo($id); ?>url" value="<?php echo($url); ?>">
+        <input style="display:none;" type="text" readonly id="<?php echo($id); ?>phpbb" value="<?php echo($phpbb); ?>">
+<?php
+        if($autogallery){
+            /*
+            <button onclick="copy('<?php echo($id); ?>autogallery')">gallery</button>
+            <input style="display:none;" type="text" readonly id="<?php echo($id); ?>autogallery" value="<?php echo($autogallery); ?>">
+             */
+?>
+            <a href="<?php echo($autogallery); ?>">gallery</a>
+<?php
+        }
+?>
+        <br />
+<?php
+        }
     }
 }
-?></pre>
 
-<?php
-
-if(isset($filenames_to_copy)){
-    $full_fname = $filenames_to_copy[0][1];
-    $full_url = $MY_URL . rawurlencode($filenames_to_copy[0][1]);
-?>
-    <label><a href="<?php echo($full_url); ?>"><?php echo($full_fname); ?></a></label><br>
-    <label>URL:</label>
-<?php
-    foreach($filenames_to_copy as $fname){
-        $display = $fname[0];
-        $name = $MY_URL . rawurlencode($fname[1]);
-?>
-        <input style="display:none;" type="text" readonly id="<?php echo($display); ?>url" value="<?php echo($name); ?>">
-        <button onclick="copy('<?php echo($display); ?>url')"><?php echo($display); ?></button>
-<?php
-    }
-?>
-
-    <br><label>[img]:</label>
-<?php
-    foreach($filenames_to_copy as $fname){
-        $display = $fname[0];
-        $name = "[img]" . $MY_URL . rawurlencode($fname[1]) . "[/img]";
-?>
-        <input style="display:none;" type="text" readonly id="<?php echo($display); ?>phpbb" value="<?php echo($name); ?>">
-        <button onclick="copy('<?php echo($display); ?>phpbb')"><?php echo($display); ?></button>
-<?php
-    }
-?>
-<hr>
-<?php
-}
-?>
-
-<form method="post" enctype="multipart/form-data">
-<label>Secret</label><br>
-<input type="text" <?php echo($passwd1_default); ?> id="passwd1" name="passwd1"><br>
-<input type="text"<?php echo($passwd2_default); ?> id="passwd2" name="passwd2"><br>
-
-<label for="rename">Rename?</label><br>
-<input type="text"<?php echo($rename_default); ?> id="rename" name="rename" autocapitalize="off"><br>
-
-<label for="sequence">Sequence?</label><br>
-<input type="number"<?php echo($sequence_default); ?> id="sequence" name="sequence"><br>
-
-<label for="overwrite">Overwrite?</label>
-<input type="checkbox" id="overwrite" name="overwrite"<?php echo($overwrite_default); ?>><br>
-
-<label for="add_date">Date?</label>
-<input type="checkbox" id="add_date" name="add_date"<?php echo($add_date_default); ?>><br>
-
-<input type="file" id="img" name="img" accept="image/png,image/jpeg,image/webp,image/gif" /><br>
-
-<input type="submit" value="Submit" />
-
-</form>
-
-<?php
-if($do_list){
-?>
-<hr>
-<?php
-    $idx = 0;
-    foreach(glob("*.{jpg,png,webp,gif}", GLOB_BRACE) as $filename){
+function do_gallery()
+{
+    global $MY_URL;
+    $gal_str = uncompress($_GET["g"]);
+    $filenames = explode(",", $gal_str);
+    foreach($filenames as $filename){
         $url = $MY_URL . rawurlencode($filename);
-        $phpbb = "[img]" . $url . "[/img]";
-        $id = "btn" . $idx;
-        $idx++;
 ?>
-    <label><a href="<?php echo($url); ?>"><?php echo($filename); ?></a></label>
-    <button onclick="copy('<?php echo($id); ?>url')">URL</button>
-    <button onclick="copy('<?php echo($id); ?>phpbb')">[img]</button>
-    <input style="display:none;" type="text" readonly id="<?php echo($id); ?>url" value="<?php echo($url); ?>">
-    <input style="display:none;" type="text" readonly id="<?php echo($id); ?>phpbb" value="<?php echo($phpbb); ?>"><br>
-
+        <img src="<?php echo($url); ?>" /><br /><br />
 <?php
     }
 }
+
+if(isset($_GET["g"])){
+    do_gallery();
+}else{
+    do_main();
+}
+
 ?>
 
 </body>
